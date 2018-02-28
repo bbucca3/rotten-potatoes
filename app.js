7c6f2b940aebea81edb86accae78f8db2643bade
Benjamin Bucca <benjamin.bucca3@gmail.com>
2018-02-28 12:10:05 -0800

    env port

--- app.js
+++ app.js
@@ -79,6 +79,8 @@ app.delete('/reviews/:id', function (req, res) {
   })
 })
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000')
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
 })
